495d5edb85b66796905d2abdb03d2949e02408b2
anvi08 <anvi8aj@gmail.com>
2020-10-19 10:32:28 +0530

    Added my project

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import Card from './Card';
+import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
-
+import 'tachyons';
 ReactDOM.render(
   <React.StrictMode>
     <App />
